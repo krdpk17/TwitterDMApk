8f52b16f69ae0582eabbda0ebbd3fdf12344a25f
krdpk1717 <62544105+krdpk1717@users.noreply.github.com>
2020-04-18 16:52:36 +0530

    Added read and write to file code

--- app/src/main/java/com/example/twitterdmcode/MainActivity.java
+++ app/src/main/java/com/example/twitterdmcode/MainActivity.java
@@ -3,12 +3,62 @@ package com.example.twitterdmcode;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView textView;
+    EditText editText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editText = (EditText) findViewById(R.id.editText);
+        textView = (TextView) findViewById(R.id.textView);
+        textView.setVisibility(View.GONE);
+    }
+
+    public void writeMessage(View view){
+        String Message = editText.getText().toString();
+        String file_name = "config_file";
+        try {
+            FileOutputStream fileOutputStream = openFileOutput(file_name, MODE_PRIVATE);
+            fileOutputStream.write(Message.getBytes());
+            fileOutputStream.close();
+            Toast.makeText(getApplicationContext(), "Message saved", Toast.LENGTH_LONG).show();
+            editText.setText("");
+        }catch (FileNotFoundException e){
+            e.printStackTrace();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public void readMessage(View view){
+        try {
+            String Message;
+            FileInputStream fileInputStream = openFileInput("config_file");
+            InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream);
+            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+            StringBuffer stringBuffer = new StringBuffer();
+            while ((Message = bufferedReader.readLine()) != null) {
+                stringBuffer.append(Message+"\n");
+            }
+            textView.setText(stringBuffer.toString());
+            textView.setVisibility(View.VISIBLE);
+        }catch(FileNotFoundException e){
+            e.printStackTrace();
+        }catch(IOException e){
+            e.printStackTrace();
+        }
     }
 }
